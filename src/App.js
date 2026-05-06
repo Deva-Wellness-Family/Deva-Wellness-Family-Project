9db8e556e850481e6799bbfa1f97234cde54efc8
SIT2711 <your-github-email@example.com>
2026-05-06 19:10:50 +0530

    Fix video deployment issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"; // Video import fixed
 import "./App.css";
 import { motion, AnimatePresence } from "framer-motion";
 import { FaWhatsapp } from "react-icons/fa";
